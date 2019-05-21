e5e7a0a4ff78a3bb8ba1d1396ed93eb832c09db6
Malyellen Vang <50208740+MalyElle@users.noreply.github.com>
2019-05-20 19:28:17 -0500

    Being more Creative and adding in W3css into JS

--- restApi.js
+++ restApi.js
@@ -1,8 +1,19 @@
 var grootDiv = document.getElementById('groot');
+
+var backgroundColor = document.body.style.backgroundColor = "teal";
+
 var container = document.createElement('div');
 
 var logo = document.createElement('img');
-logo.src='grootImg.jpg';
+logo.src='Hayao_Miyazaki.jpg';
+logo.style.display = 'block'; // How to put it in the middle
+logo.style.marginLeft = 'auto';
+logo.style.marginRight = 'auto';
+logo.style.height = '600px';
+logo.style.width = '800px';
+logo.classList.add('w3-white','w3-border',)
+logo.style.borderWidth = '10px';
+
 
 grootDiv.appendChild(container);
 container.appendChild(logo);
@@ -21,14 +32,28 @@ request.onload = function() {
             var card = document.createElement('div');
             var heading1 = document.createElement('h1');
             heading1.textContent = film.title;
+            heading1.style.textAlign = 'center';
+            heading1.style.color = 'white';
+            heading1.classList.add('w3-margin','w3-black', 'w3-border')
 
             var para = document.createElement('p');
-            film.description = film.description.substring(0,300);
+            film.description = film.description.substring(0,400);
             para.textContent = `${film.description}`;
+            para.style.fontSize = '1.2rem';
+            para.classList.add('w3-border', 'w3-white', 'w3-margin')
 
+            var director = document.createElement('p');
+            director.textContent='Director Name: '+film.director;
+            director.style.textAlign = 'center';
+            director.style.fontSize = '1.5rem';
+            director.style.color = 'silver';
+            
+            
             container.appendChild(card);
             card.appendChild(heading1);
+            card.appendChild(director);
             card.appendChild(para);
+           
         });
     } else {
         alert('Oh Geez');
